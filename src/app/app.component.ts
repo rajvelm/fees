4283a8b767242c7241fc4c885c61b88d7b8546c6
rajvel <rajvel.m@mxp.awsapps.com>
2018-06-14 14:50:23 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-//import { LicenseManager } from 'ag-grid-enterprise/main';
-//LicenseManager.setLicenseKey('Preflex_Solutions_Pvt._Ltd._on_behalf_of_Societe_Generale_MultiApp_4Devs25_May_2019_MTU1ODczODgwMDAwMA==5c5c950bb1ce349fe73cb689a1b495ed');
 
 @Component({
   selector: 'app-root',
